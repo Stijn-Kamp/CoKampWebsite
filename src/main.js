1c41abd4fb54ce2590813aa8d551b70ef3f6afce
Stijn Kamp <skamp@softslings.com>
2022-11-12 15:31:23 +0100

    added bootstrap

--- src/main.js
+++ src/main.js
@@ -1,8 +1,12 @@
 import { createApp } from "vue";
-import App from "./App.vue";
-import router from "./router";
+import App from "~/App.vue";
+import router from "~/router";
 
-import "./assets/main.css";
+// Bootstrap
+import "bootstrap/dist/css/bootstrap.min.css"
+import "bootstrap"
+
+import "~/assets/main.css";
 
 const app = createApp(App);
 

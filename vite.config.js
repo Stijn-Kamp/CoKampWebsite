559c81fe74f2f298585c3c8cd1bd3a177a727cec
Stijn Kamp <skamp@softslings.com>
2022-11-12 15:27:32 +0100

    added aliases to vite config

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,20 @@
-import { fileURLToPath, URL } from "node:url";
-
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
+import path from "path";
+
+const __dirname = path.dirname("./src");
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
   resolve: {
     alias: {
-      "@": fileURLToPath(new URL("./src", import.meta.url)),
+      "~": path.dirname("./src"),
+      "@": path.resolve(__dirname, "./src"),
+      "@components": path.resolve(__dirname, "./src/components"),
+      "@views": path.resolve(__dirname, "./src/views"),
+      "@assets": path.resolve(__dirname, "./src/assets"),
     },
+    extensions: ["cjs", ".js", ".vue", ".json"],
   },
+  plugins: [vue()],
 });

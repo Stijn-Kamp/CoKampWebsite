f26bfeb85b0169f9cd8bb41a9210b98b49c53287
Stijn Kamp <skamp@softslings.com>
2022-11-14 23:29:29 +0100

    updated logo

--- src/components/TitleBar.vue
+++ src/components/TitleBar.vue
@@ -11,8 +11,12 @@
 
 <template>
     <header class="masthead">
-      <div class="col-20 text-center align-items-center">
-        <CoKamp/>
+      <div class="container h-100">
+        <div class="row h-50 align-items-center">
+          <div class="col-20 text-center">
+            <CoKamp/>
+          </div>
+        </div>
       </div>
     </header>
 </template>
